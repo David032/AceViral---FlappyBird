64a7ad742c2bd4f3bf35cc545c95c8e6e7e94dfe
David K <davek032@outlook.com>
2022-01-26 11:52:30 +0000

    Android & Height Controls
    *Tested it works on android
    *Added height deviation limits to prevent extreme variations

--- Assets/Game/Systems/ObjectSpawner.cs
+++ Assets/Game/Systems/ObjectSpawner.cs
@@ -12,15 +12,27 @@ public class ObjectSpawner : MonoBehaviour
     float ColumnMin = 4;
     float ColumnMax = 8.5f;
     float ColumnSpacing = 5;
+    float ColumnHeightDeviation = 1.5f;
     Vector3 NextPosition;
+    float LastColumnHeight;
 
     void Start()
     {
         NextPosition = StartPosition;
+        float randomHeight;
         for (int i = 0; i < InitialPillars; i++)
         {
             GameObject NewColumn = Instantiate(Column, ColumnsHolder.transform);
-            float randomHeight = Random.Range(ColumnMin, ColumnMax);
+            if (i == 0)
+            {
+                randomHeight = Random.Range(ColumnMin, ColumnMax);
+            }
+            else
+            {
+                randomHeight = 
+                    GenerateConstrainedRandomPosition(LastColumnHeight);
+            }
+            LastColumnHeight = randomHeight;
             NewColumn.transform.position = NextPosition;
             NewColumn.transform.Translate(new Vector3(0, randomHeight, 0));
 
@@ -31,10 +43,18 @@ public class ObjectSpawner : MonoBehaviour
     public IEnumerator MovePillarUp(GameObject pillarToMove) 
     {
         yield return new WaitForSeconds(1.25f);
-        float randomHeight = Random.Range(ColumnMin, ColumnMax);
+        float randomHeight = 
+            GenerateConstrainedRandomPosition(LastColumnHeight);
         pillarToMove.transform.position = NextPosition;
         pillarToMove.transform.Translate(new Vector3(0, randomHeight, 0));
         NextPosition += new Vector3(0, 0, ColumnSpacing);
         yield return new WaitForSeconds(0.25f);
+    }
+
+    float GenerateConstrainedRandomPosition(float lastHeight) 
+    {
+        float randomHeight = Random.Range(lastHeight-ColumnHeightDeviation/2,
+            lastHeight + ColumnHeightDeviation/2);
+        return randomHeight;
     }
 }
